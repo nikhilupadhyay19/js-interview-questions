9404f5ac6c43c9712df8af3c0bbb1b083700c58b
nikhilupadhyay19 <55686365+nikhilupadhyay19@users.noreply.github.com>
2022-08-14 12:42:14 +0530

    Initial commit

---
+++ src/helper/helperFn.js
@@ -0,0 +1,11 @@
+const helperFn = () => {
+  // Reduce Method use cases
+
+  const services = Object.freeze({});
+
+  return services;
+};
+
+const helperFns = helperFn();
+
+export { helperFns };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles.css";
+import { helperFns } from "./helper/helperFn.js";
+
+console.log(helperFns);
